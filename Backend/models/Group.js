54740e2d4cdb36c00a53f4e2b4902b50004f0dc3
Rahul Prajapati <rahul.prajapati@cubastion.com>
2026-01-25 00:50:03 +0530

    Created DB schema

---
+++ Backend/models/Group.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model("Group", groupSchema);
